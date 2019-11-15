1702d2bf2a970855c2e626f49339dc0f67ed56f5
Indian966 <rlaalsrb4175@gmail.com>
2019-11-16 01:24:30 +0900

    Update TSP_DP.py
    DP방법 완성

--- TSP_DP.py
+++ TSP_DP.py
@@ -1,34 +1,32 @@
-# 재귀 없이
-from pprint import pprint
-
 INF = 10000
 dist = [[0, 2, 9, INF],
         [1, 0, 6, 4],
         [INF, 7, 0, 8],
         [6, 3, INF, 0]]
 N = len(dist)
-
 path = [0]
 cost = 0
 VISITED_ALL = (1 << N)-1 # 1111
-visited = 1 << 4 #1000
+visited = 1 << 0 #1
 
-result = []
 def tsp_dp (path, visited, cost) :
     inf = float('inf')
-    ans = inf
-    memo = [[0 for x in range(N-1)] for x in range(N)]
+    memo = [[None] * (1 << N) for _ in range(N)]
 
+    def subpath(end, visited) :
+        if visited == VISITED_ALL :
+            return dist[end][0] or inf
+        if memo[end][visited] is not None:
+            return memo[end][visited]
 
-    def subpath (start, end) :
-        if dist[start][end] != 0 and dist[start][end] < inf :
-            return dist[start][end]
-        elif dist[start][end] == inf :
-            for i in range(N) :
-                ans = min(subpath(start,i) + subpath(i,end))
-                return ans
-    pprint(path)
+        temp_c = inf
+        for city in range(N):
+            if visited & 1 << city: continue
+            if visited & (1 << city) == 0 and dist[end][city] !=0 :
+                temp_c = min(temp_c, subpath(city, visited | (1 << city)) + dist[end][city])
+        memo[end][city] = temp_c
+        return temp_c
 
-# print(tsp_dp(path, visited, cost))
+    return subpath(0, visited)
 
-tsp_dp(path, visited, cost)
+print(tsp_dp(path, visited, cost))
